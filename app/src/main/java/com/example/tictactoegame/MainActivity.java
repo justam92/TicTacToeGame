5e73f83e08a3dcecf6ba5d328fe0095d5525b66c
justam92 <31111330+justam92@users.noreply.github.com>
2019-12-22 22:55:13 +0100

    Added service GameBoardService

--- app/src/main/java/com/example/tictactoegame/MainActivity.java
+++ app/src/main/java/com/example/tictactoegame/MainActivity.java
@@ -1,35 +1,37 @@
 package com.example.tictactoegame;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.tictactoegame.model.Player;
+import com.example.tictactoegame.service.GameBoardService;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private GameBoardService gameBoardService = new GameBoardService();
+
     private Button[][] buttons = new Button[3][3];
 
-    private boolean player1Turn = true;
+    private Boolean player1Turn = true;
 
-    private int roundCount;
+    private Integer roundCount = 0;
 
-    private int player1Points;
-    private int player2Points;
+    private Player player1 = new Player();
+    private Player player2 = new Player();
 
-    private TextView textViewPlayer1;
-    private TextView textViewPlayer2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textViewPlayer1 = findViewById(R.id.text_view_p1);
-        textViewPlayer2 = findViewById(R.id.text_view_p2);
+        player1.setTextView((TextView) findViewById(R.id.text_view_p1));
+        player2.setTextView((TextView) findViewById(R.id.text_view_p2));
 
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
@@ -44,18 +46,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         buttonReset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                resetGame();
+                gameBoardService.resetGame(player1, player2, buttons, roundCount, player1Turn);
             }
         });
     }
 
     @Override
     public void onClick(View v) {
-        if (!((Button) v).getText().toString().equals("")){
+        if (!((Button) v).getText().toString().equals("")) {
             return;
         }
 
-        if (player1Turn){
+        if (player1Turn) {
             ((Button) v).setText("X");
         } else {
             ((Button) v).setText("O");
@@ -63,96 +65,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         roundCount++;
 
-        if (checkForWin()){
-            if (player1Turn){
-                player1Wins();
+        if (gameBoardService.checkForWin(buttons)) {
+            if (player1Turn) {
+                gameBoardService.player1Wins(player1, this, buttons, roundCount, player1Turn);
             } else {
-                player2Wins();
+                gameBoardService.player2Wins(player2, this, buttons, roundCount, player1Turn);
             }
-        } else if (roundCount == 9){
-            draw();
+        } else if (roundCount == 9) {
+            gameBoardService.draw(this, buttons, roundCount, player1Turn);
         } else {
             player1Turn = !player1Turn;
         }
 
-    }
-
-    private boolean checkForWin(){
-        String[][] field = new String[3][3];
-
-        for(int i = 0; i <3; i++){
-            for(int j = 0; j < 3; j++){
-                field[i][j] = buttons[i][j].getText().toString();
-            }
-        }
-
-        for(int i = 0; i < 3; i++){
-            if(field[i][0].equals(field[i][1]) && field[i][0].equals(field[i][2]) && !field[i][0].equals("")){
-                return true;
-            }
-        }
-
-        for(int i = 0; i < 3; i++){
-            if(field[0][i].equals(field[1][i]) && field[0][i].equals(field[2][i]) && !field[0][i].equals("")){
-                return true;
-            }
-        }
-
-        if(field[0][0].equals(field[1][1]) && field[0][0].equals(field[2][2]) && !field[0][0].equals("")){
-            return true;
-        }
-
-        if(field[0][2].equals(field[1][1]) && field[0][2].equals(field[2][0]) && !field[0][2].equals("")){
-            return true;
-        }
-
-        return false;
-
-    }
-
-    private void player1Wins(){
-        player1Points++;
-
-        Toast.makeText( this, "Player 1 wins!", Toast.LENGTH_SHORT).show();
-        updatePointsText();
-        resetBoard();
-    }
-
-    private void player2Wins(){
-        player2Points++;
-
-        Toast.makeText( this, "Player 2 wins!", Toast.LENGTH_SHORT).show();
-        updatePointsText();
-        resetBoard();
-    }
-
-    private void draw(){
-        Toast.makeText(this, "Draw!", Toast.LENGTH_SHORT).show();
-        resetBoard();
-    }
-
-    private void updatePointsText(){
-        textViewPlayer1.setText("Player 1: " + player1Points);
-        textViewPlayer2.setText("Player 2: " + player2Points);
-    }
-
-    private void resetBoard(){
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j <3; j++){
-                buttons[i][j].setText("");
-            }
-        }
-
-        roundCount = 0;
-
-        player1Turn = true;
-    }
-
-    private void resetGame(){
-        player1Points = 0;
-        player2Points = 0;
-        updatePointsText();
-        resetBoard();
     }
 
     @Override
@@ -160,8 +84,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onSaveInstanceState(outState);
 
         outState.putInt("roundCount", roundCount);
-        outState.putInt("player1Points", player1Points);
-        outState.putInt("player2Points", player2Points);
+        outState.putInt("player1Points", player1.getPoints());
+        outState.putInt("player2Points", player2.getPoints());
         outState.putBoolean("player1Turn", player1Turn);
     }
 
@@ -170,8 +94,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onRestoreInstanceState(savedInstanceState);
 
         roundCount = savedInstanceState.getInt("roundCount");
-        player1Points = savedInstanceState.getInt("player1Points");
-        player2Points = savedInstanceState.getInt("player2Points");
+        player1.setPoints(savedInstanceState.getInt("player1Points"));
+        player2.setPoints(savedInstanceState.getInt("player2Points"));
         player1Turn = savedInstanceState.getBoolean("player1Turn");
     }
 }
--- app/src/main/java/com/example/tictactoegame/service/GameBoardService.java
+++ app/src/main/java/com/example/tictactoegame/service/GameBoardService.java
@@ -1,4 +1,96 @@
 package com.example.tictactoegame.service;
 
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.tictactoegame.MainActivity;
+import com.example.tictactoegame.model.Player;
+
 public class GameBoardService {
+
+    public boolean checkForWin(Button[][] buttons) {
+        String[][] field = new String[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                field[i][j] = buttons[i][j].getText().toString();
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (field[i][0].equals(field[i][1]) && field[i][0].equals(field[i][2]) && !field[i][0].equals("")) {
+                return true;
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (field[0][i].equals(field[1][i]) && field[0][i].equals(field[2][i]) && !field[0][i].equals("")) {
+                return true;
+            }
+        }
+
+        if (field[0][0].equals(field[1][1]) && field[0][0].equals(field[2][2]) && !field[0][0].equals("")) {
+            return true;
+        }
+
+        if (field[0][2].equals(field[1][1]) && field[0][2].equals(field[2][0]) && !field[0][2].equals("")) {
+            return true;
+        }
+
+        return false;
+
+    }
+
+    public void player1Wins(Player player1, MainActivity mainActivity, Button[][] buttons, Integer roundCount, Boolean player1Turn) {
+        player1.setPoints(player1.getPoints() + 1);
+        updatePlayer1PointsText(player1);
+        Toast.makeText(mainActivity, "Player 1 wins!", Toast.LENGTH_SHORT).show();
+        resetBoard(buttons, roundCount, player1Turn);
+    }
+
+    public void player2Wins(Player player2, MainActivity mainActivity, Button[][] buttons, Integer roundCount, Boolean player1Turn) {
+        player2.setPoints(player2.getPoints() + 1);
+
+        Toast.makeText(mainActivity, "Player 2 wins!", Toast.LENGTH_SHORT).show();
+        updatePlayer2PointsText(player2);
+        resetBoard(buttons, roundCount, player1Turn);
+    }
+
+    public void draw(MainActivity mainActivity, Button[][] buttons, Integer roundCount, Boolean player1Turn) {
+        Toast.makeText(mainActivity, "Draw!", Toast.LENGTH_SHORT).show();
+        resetBoard(buttons, roundCount, player1Turn);
+    }
+
+    public void resetGame(Player player1, Player player2, Button[][] buttons, Integer roundCount, Boolean player1Turn) {
+        player1.setPoints(0);
+        player2.setPoints(0);
+        updatePointsText(player1, player2);
+        resetBoard(buttons, roundCount, player1Turn);
+    }
+
+    private void updatePointsText(Player player1, Player player2) {
+        updatePlayer1PointsText(player1);
+
+        updatePlayer2PointsText(player2);
+    }
+
+    private void updatePlayer1PointsText(Player player1) {
+        player1.getTextView().setText("Player 1: " + player1.getPoints());
+    }
+
+    private void updatePlayer2PointsText(Player player2) {
+        player2.getTextView().setText("Player 2: " + player2.getPoints());
+    }
+
+    private void resetBoard(Button[][] buttons, Integer roundCount, Boolean player1Turn) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j].setText("");
+            }
+        }
+
+        roundCount = 0;
+
+        player1Turn = true;
+    }
 }
